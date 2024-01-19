a96ef75fca156350af0841abf11d2fcc8305adf3
Haider Zaidi <115361024+haider-zaidi@users.noreply.github.com>
2024-01-19 12:40:45 +0000

    01_Program

---
+++ 01_variable.js
@@ -0,0 +1,15 @@
+console.log("Hello Guyz")
+var a=4
+console.log(a)
+a="string"
+console.log(a)
+var b="Good"
+{
+    let b="Morning"
+    console.log(b)
+}
+console.log(b)
+const name="Haider"
+// name="Abc"  --> Throws an eror bcz const cannot be changed throughout the program.
+let c=null
+let x=undefined
